01c353a05248ed43c6d9a51d4b7d8744d1c98075
ADMIN <ADMIN@PC330>
2025-07-24 15:37:25 +0700

    thinhgjh

--- src/main/java/Phan_1/Bai_Tap_2.java
+++ src/main/java/Phan_1/Bai_Tap_2.java
@@ -4,10 +4,23 @@
  */
 package Phan_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
-public class Bai_Tap_2 {
+public class Bai_Tap_2 {public static void main (String[] args){
+    int a,b;
+    int dientich;
+    Scanner sc= new Scanner(System.in);
+    System.out.print("cho biet chieu dai la:" );
+    a=sc.nextInt();
+    System.out.print("cho biet chieu rong:");
+    b=sc.nextInt();
+    
+    dientich=a*b;
+    System.out.println(" dien tich hinh chu nhat la:"+ dientich);
+    }
     
 }
--- src/main/java/Phan_1/Bai_Tap_3.java
+++ src/main/java/Phan_1/Bai_Tap_3.java
@@ -4,10 +4,28 @@
  */
 package Phan_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
-public class Bai_Tap_3 {
+public class Bai_Tap_3 {public static void main (String[] args){
+    double a,b;
+    double nghiem;
+    Scanner sc= new Scanner(System.in);
+    System.out.print("cho biet so thu 1 la:" );
+    a=sc.nextDouble();
+    System.out.print("cho biet so thu 2:");
+    b=sc.nextDouble();
+    
+    if (a==0)
+    {
+        if (b==0)
+            
+                
+    }
+    System.out.println("tong 2 so la:"+ nghiem);
+    }
     
 }
