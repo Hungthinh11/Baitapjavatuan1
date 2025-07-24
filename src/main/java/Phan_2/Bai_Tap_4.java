58f33f7f779f45ae8b5aac489d23c1c2683be33c
ADMIN <ADMIN@PC330>
2025-07-24 16:22:08 +0700

    thinhhung

--- src/main/java/Phan_2/Bai_Tap_4.java
+++ src/main/java/Phan_2/Bai_Tap_4.java
@@ -4,10 +4,29 @@
  */
 package Phan_2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
 public class Bai_Tap_4 {
+    public static void main (String[] args){
+        int a;
+    Scanner sc= new Scanner(System.in);
+    System.out.print("nhap so nguyen: " );
+    a=sc.nextInt();
+ 
     
+    if (a % 2 ==0)
+    {
+       System.out.println("day la so chan");
+    }
+        else{
+     System .out.print("day la so le");
 }
+ 
+    }
+}
+
+
--- src/main/java/Phan_2/Bai_Tap_5.java
+++ src/main/java/Phan_2/Bai_Tap_5.java
@@ -4,10 +4,33 @@
  */
 package Phan_2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
-public class Bai_Tap_5 {
-    
+public class Bai_Tap_5 {public static void main (String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.print("nhap so a:" );
+   int a=sc.nextInt();
+    System.out.print("nhap so b:");
+   int b=sc.nextInt();
+    System.out.print("nhap so c:");
+   int c=sc.nextInt();
+    int max;
+    if(a>b & a>c)
+    {
+        max = a;
+    }
+    else if(b>a & b>c)
+    {
+        max=b;
+    }
+    else
+        {
+            max=c;
+        }
+    System.out.print("so lon nhat la:"+ max);
+}
 }
